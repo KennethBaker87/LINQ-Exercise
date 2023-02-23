7ff76031cfee8f6e6563c98c16acbfb8209c98ea
Kenneth Baker <spyksaturn@gmail.com>
2023-02-23 10:12:34 -0500

    Initial Commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //create a list of video game names
+            //order the list by legnth of game name using lambda method syntax
+            List<string> videoGames = new List<string> { "Ninja Gaiden", "Golden Eye", "Castlevania", "PilotWings 64"};
+            videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
